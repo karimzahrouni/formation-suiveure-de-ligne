d93025a4e1cca84dcffd1a7967359f29b8ea0537
karimzahrouni <35636080+karimzahrouni@users.noreply.github.com>
2019-03-04 17:56:31 +0100

    Rename TESTE_motor.ino to teste motor/TESTE_motor.ino

---
+++ teste motor/TESTE_motor.ino
@@ -0,0 +1,26 @@
+
+
+int pin1=3;
+int pin2=4;
+int pin3=8;
+int pin4=9;
+
+
+void setup() {
+pinMode(pin1,OUTPUT);
+pinMode(pin2,OUTPUT);
+pinMode(pin3,OUTPUT);
+pinMode(pin4,OUTPUT);
+
+}
+
+void loop() {
+
+digitalWrite (pin1,HIGH);
+digitalWrite(pin2,LOW);
+digitalWrite (pin4,HIGH);
+digitalWrite(pin3,LOW);
+
+
+
+}
